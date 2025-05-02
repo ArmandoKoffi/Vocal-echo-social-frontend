f05afb0484b343c70da5617bd6e000ce42caaa30
ArmandoKoffi <armandokoffikoffi@gmail.com>
2025-05-02 14:37:30 +0000

    Changement de l'URL pour le backend render

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://vocal-echo-social-backend.onrender.com/api"; 
 
-// Configuration d'Axios
+// Configuration d'Axios 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
